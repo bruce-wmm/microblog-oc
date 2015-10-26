c9590dfab4b31b2867e8c7973d548824ba54650f
6ag <admin@6ag.cn>
2015-10-26 08:45:27 +0800

    Initial Commit

---
+++ microblog-oc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  microblog-oc
+//
+//  Created by jianfeng on 15/10/26.
+//  Copyright © 2015年 六阿哥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ microblog-oc/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  microblog-oc
+//
+//  Created by jianfeng on 15/10/26.
+//  Copyright © 2015年 六阿哥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
